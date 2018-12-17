1df3d762fd57db3657b0a39df59ed6414e583c23
turing <463387427@qq.com>
2018-12-17 21:57:30 +0800

    添加vim依赖软件,和bundle.

--- vim_install.sh
+++ vim_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ~
+sudo apt-get install libncurses5-dev
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/vim/vim.git
 cd ~/vim
 ./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-pythoninterp=yes --enable-python3interp=yes --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu --enable-perlinterp=yes --enable-luainterp=yes --enable-cscope --prefix=/usr
